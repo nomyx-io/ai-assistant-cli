9d090be701fed89d71b26b3068d6312808ba0e0e
Sebastian Schepis <sschepis@gmail.com>
2024-02-12 01:37:03 -0700

    initial

--- publish.sh
+++
@@ -1,3 +0,0 @@
-cd ../ai-assistant && rm -rf dist && npm run build && npm publish --access public && git add . && git commit -m "publish" && git push
-cd ../ai-assistant-tools && rm -rf dist && npm run build && npm publish --access public && git add . && git commit -m "publish" && git push
-cd ../ai-assistant-cli && rm -rf dist && npm run build && npm publish --access public && git add . && git commit -m "publish" && git push
